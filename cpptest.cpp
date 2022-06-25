122662857809257bdea1438bff1a0c334cafa6a7
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2022-06-24 22:52:31 -0400

    Added operator overloads

--- cpptest.cpp
+++ cpptest.cpp
@@ -11,16 +11,28 @@ public:
         return name;
     }
     void setName(wstring nname);
+    void operator+(int n) {
+        age = age + n;
+        cout<<"Incrementing age!"<<endl;
+    }
+    void operator-(int n) {
+        age = age - n;
+        cout<<"Decrementing age by "<<n<<"."<<endl;
+    }
+    int getAge();
 private:
     wstring name;
+    int age;
 
 };
 Dog::Dog(wstring nname) {
     this->name = nname;
+    this->age = 0;
 }
 Dog::Dog()
 {
     name = L"Unknown";
+    age = 0;
 }
 
 Dog::~Dog()
@@ -31,17 +43,25 @@ Dog::~Dog()
 void Dog::setName(wstring nname) {
     this->name = nname;
 }
+int Dog::getAge() {
+    return this->age;
+}
 int main(int ac, char *av[]) {
     Dog Murphy;
     Dog mouse(L"Mouse Dog");
     Dog* spot = new Dog(L"Spot");
     wcout<<"Spot name is "<<spot->getName()<<endl;
-    delete spot;
     wcout<<spot->getName()<<endl;
     wcout<<Murphy.getName()<<endl;
     Murphy.setName(L"Murphy Dog");
     wcout<<"Murphy newname = "<<Murphy.getName()<<endl;
+    wcout<<"M age = "<<Murphy.getAge()<<endl;
+    Murphy + 1;
+    wcout<<"M age = "<<Murphy.getAge()<<endl;
+    Murphy - 3;
+    wcout<<"M age = "<<Murphy.getAge()<<endl;
     wcout<<mouse.getName()<<endl;
     cout<<"Hello, World"<<endl;
+    delete spot;
 }
 
